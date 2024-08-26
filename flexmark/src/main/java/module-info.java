aee87178a5d24dbfb56cc1b88318cba55652ea04
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-08-26 21:16:32 +0400

    add kotlin sdk plugin
    reformat

--- flexmark/src/main/java/module-info.java
+++ flexmark/src/main/java/module-info.java
@@ -2,19 +2,18 @@
  * @author VISTALL
  * @since 16-Aug-22
  */
-module consulo.library.flexmark
-{
-	requires transitive flexmark;
-	requires transitive flexmark.util;
-	requires transitive flexmark.util.ast;
-	requires transitive flexmark.util.builder;
-	requires transitive flexmark.util.collection;
-	requires transitive flexmark.util.data;
-	requires transitive flexmark.util.dependency;
-	requires transitive flexmark.util.format;
-	requires transitive flexmark.util.html;
-	requires transitive flexmark.util.misc;
-	requires transitive flexmark.util.options;
-	requires transitive flexmark.util.sequence;
-	requires transitive flexmark.util.visitor;
+module consulo.library.flexmark {
+    requires transitive flexmark;
+    requires transitive flexmark.util;
+    requires transitive flexmark.util.ast;
+    requires transitive flexmark.util.builder;
+    requires transitive flexmark.util.collection;
+    requires transitive flexmark.util.data;
+    requires transitive flexmark.util.dependency;
+    requires transitive flexmark.util.format;
+    requires transitive flexmark.util.html;
+    requires transitive flexmark.util.misc;
+    requires transitive flexmark.util.options;
+    requires transitive flexmark.util.sequence;
+    requires transitive flexmark.util.visitor;
 }
---
+++ kotlin-sdk/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author VISTALL
+ * @since 2024-08-26
+ */
+module consulo.library.kotlin.sdk {
+    requires transitive kotlin.reflect;
+    requires transitive kotlin.stdlib;
+    requires transitive kotlin.annotations.jvm;
+}
