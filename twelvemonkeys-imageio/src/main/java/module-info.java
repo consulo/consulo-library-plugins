6ae49767063d6ee59ea9ec984a56a800f54c8e6d
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-12-27 13:13:15 +0200

    add twelvemonkeys image io library plugin

---
+++ twelvemonkeys-imageio/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author VISTALL
+ * @since 27/12/2025
+ */
+module consulo.library.twelvemonkeys.imageio {
+    requires com.twelvemonkeys.common.io;
+    requires com.twelvemonkeys.common.lang;
+    requires com.twelvemonkeys.common.image;
+}
