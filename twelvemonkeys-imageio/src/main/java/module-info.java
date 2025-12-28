248bc699160d9d38bba44ec90ded210252bdf240
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-12-28 18:05:14 +0200

    make library plugin empty if it's not desktop awt platform

--- twelvemonkeys-imageio/src/main/java/module-info.java
+++ twelvemonkeys-imageio/src/main/java/module-info.java
@@ -3,7 +3,5 @@
  * @since 27/12/2025
  */
 module consulo.library.twelvemonkeys.imageio {
-    requires com.twelvemonkeys.common.io;
-    requires com.twelvemonkeys.common.lang;
-    requires com.twelvemonkeys.common.image;
+    requires consulo.component.api;
 }
